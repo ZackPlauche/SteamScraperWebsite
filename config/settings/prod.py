4f6908939e17d75a4bdea1dab3446ec7bb17cc83
Zack Plauché <zackknowspython@gmail.com>
2022-02-03 17:54:33 +0200

    Add logging

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -10,4 +10,32 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['steamscraperwebsite.herokuapp.com']
 
+DEBUG_PROPAGATE_EXCEPTIONS = True
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'MYAPP': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    }
+}
+
 django_on_heroku.settings(locals(), staticfiles=False)
