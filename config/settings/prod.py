48ed8f92f8288da8b34fd18c1219fbc8331ff8e1
Zack Plauché <zackknowspython@gmail.com>
2022-02-03 16:56:09 +0200

    Update requirements.txt

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,13 +1,11 @@
+from decouple import config
 import django_on_heroku
 
 from .base import *
 
 
-
-
 SECRET_KEY = config('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ['steamscraperwebsite.herokuapp.com']
